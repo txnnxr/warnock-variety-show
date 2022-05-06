e393f6c248d64786b97a9cad403cc5ae696b12ef
Tanner Wells <tannerjameswells@gmail.com>
2022-05-05 22:29:21 -0400

    adds card background to thank you page

--- resources/views/shows/invites/thank-you.blade.php
+++ resources/views/shows/invites/thank-you.blade.php
@@ -1,31 +1,33 @@
 @extends('shows.layout')
 @section('shows-content')
-    <div class="card-body">
-        <h5 class="card-title">Thank you {{$invite->first_name}} {{$invite->middle_name}}
-            {{$invite->last_name}}! Your response has been saved!</h5>
-        <div class="form-control my-2">
-            <div class="row">
-                <div class="col-12">Attending?</div>
-            </div>
-            <div class="row">
-                <div class="col-12">{{$invite->response_status}}</div>
-            </div>
-        </div>
-        @if($invite->response_status == "ATTENDING")
-            <div class="talent-box form-control my-2">
-                <div class="row ">
-                    <div class="col-12">Talent?</div>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Thank you {{$invite->first_name}} {{$invite->middle_name}}
+                {{$invite->last_name}}! Your response has been saved!</h5>
+            <div class="form-control my-2">
+                <div class="row">
+                    <div class="col-12">Attending?</div>
                 </div>
                 <div class="row">
-                    <div class="col-12">
-                        @if($invite->talent)
-                            Yes
-                        @else
-                            No
-                        @endif
-                    </div>
+                    <div class="col-12">{{$invite->response_status}}</div>
                 </div>
             </div>
-        @endif
+            @if($invite->response_status == "ATTENDING")
+                <div class="talent-box form-control my-2">
+                    <div class="row ">
+                        <div class="col-12">Talent?</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            @if($invite->talent)
+                                Yes
+                            @else
+                                No
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            @endif
+        </div>
     </div>
 @endsection
