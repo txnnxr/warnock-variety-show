c8e3dd5961cdcd4bb1d00f1b270ecce6fc7f4691
Tanner Wells <tannerjameswells@gmail.com>
2022-08-13 17:54:37 -0400

    add webhook commands

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -13,10 +13,56 @@ class DeployController extends Controller
     public function deploy(Request $request)
     {
 	    Log::debug($request);
-	    $data = $request->all();
-       return (json_decode($request->input('payload'))->pusher);
+        if (json_decode($request->input('payload'))->pusher == 'txnnxr') {
+            $this->gitPull();
+            $this->composerInstall();
+            $this->npmInstall();
+            $this->migrate();
+        }
+    }
 
-//        Process::
+    private function gitPull() {
+        $process = new Process('git pull');
+        $this->info("Running 'git pull'");
 
+        $process->run(function($type, $buffer) {
+            $this->pullLog[] = $buffer;
+
+            if($buffer == "Already up to date.\n") {
+                $this->alreadyUpToDate = TRUE;
+            }
+
+        });
+
+        return $process->isSuccessful();
+    }
+
+    private function composerInstall()
+    {
+        $process = new Process('composer install --no-dev');
+        $this->info("Running 'composer install'");
+
+        $process->run(function($type, $buffer) {
+            $this->composerLog[] = $buffer;
+        });
+
+
+        return $process->isSuccessful();
+    }
+
+    private function npmInstall()
+    {
+        $process = new Process('nvm use 17');
+        $process->run();
+        $process = new Process('npm install');
+        $process->run();
+        $process = new Process('npm run prod');
+        $process->run();
+    }
+
+    private function migrate()
+    {
+        $process = new Process('php artisan migrate');
+        $process->run();
     }
 }
