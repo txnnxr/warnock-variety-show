fd4b8f649298fa59cd20bebdd447ed2a3993478d
Tanner Wells <tannerjameswells@gmail.com>
2022-08-13 18:28:34 -0400

    new pull rules

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -10,43 +10,51 @@ use Symfony\Component\Process\Process;
 
 class DeployController extends Controller
 {
+class DeployController extends Controller
+{
     public function deploy(Request $request)
     {
-	    Log::debug($request);
-        if (json_decode($request->input('payload'))->pusher->name == 'txnnxr') {
-            $this->gitPull();
-            $this->composerInstall();
-            $this->npmInstall();
-            $this->migrate();
+            Log::debug($request);
+            if (json_decode($request->input('payload'))->pusher->name == 'txnnxr') {
+                $this->gitPull();
+                $this->composerInstall();
+                $this->npmInstall();
+                $this->migrate();
         }
     }
 
     private function gitPull() {
-        $process = new Process('git pull');
-        $this->info("Running 'git pull'");
-
+        $process = new Process(['git', 'pull']);
         $process->run();
+        if (!$process->isSuccessful()) Log::error('Failed with '. $process->getExitCode());
     }
 
     private function composerInstall()
     {
-        $process = new Process('composer install --no-dev');
+        $process = new Process(['composer',  'install --no-dev']);
         $process->run();
+        if (!$process->isSuccessful()) Log::error('Failed with '. $process->getExitCode());
+
     }
 
     private function npmInstall()
     {
-        $process = new Process('nvm use 17');
+        $process = new Process(['nvm', 'use 17']);
         $process->run();
-        $process = new Process('npm install');
+        if (!$process->isSuccessful()) Log::error('Failed with '. $process->getExitCode());
+        $process = new Process(['npm', 'install']);
         $process->run();
-        $process = new Process('npm run prod');
+        if (!$process->isSuccessful()) Log::error('Failed with '. $process->getExitCode());
+        $process = new Process(['npm', 'run prod']);
         $process->run();
+        if (!$process->isSuccessful()) Log::error('Failed with '. $process->getExitCode());
     }
 
     private function migrate()
     {
         $process = new Process('php artisan migrate');
         $process->run();
+        if (!$process->isSuccessful()) Log::error('Failed with '. $process->getExitCode());
     }
+}
 }
