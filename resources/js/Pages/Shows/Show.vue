6ea761548d13576b9348ff7f916b9c37d5e07cb1
Tanner Wells <tannerjameswells@gmail.com>
2022-10-11 22:08:13 -0400

    remove modal reference and comments

--- resources/js/Pages/Shows/Show.vue
+++ resources/js/Pages/Shows/Show.vue
@@ -9,7 +9,7 @@
             <div class="card-body p-4">
                 <div class="col-md-12 my-1">
                     <Link :href="'/show/'+show.id+'/guest-invitation'">
-                        <button class="form-control btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#rsvpModal">RSVP</button>
+                        <button class="form-control btn btn-primary" type="button">RSVP</button>
                     </Link>
                 </div>
             </div>
@@ -55,74 +55,6 @@
                 </ul>
             </div>
         </div>
-
-<!--        <div class="modal fade" id="rsvpModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">-->
-<!--            <div class="modal-dialog">-->
-<!--                <div class="modal-content">-->
-<!--                    <div class="modal-header">-->
-<!--                        <h5 class="modal-title" id="exampleModalLabel">RSVP</h5>-->
-<!--                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>-->
-<!--                    </div>-->
-<!--                    <Form class="" :action="'/shows/'+show.id+'/invite/guest-request'" method="POST">-->
-<!--                        <div class="modal-body row">-->
-<!--                            <div class="col-md-6 my-1">-->
-<!--                                <input class="form-control" type="text" name="first_name" placeholder="First Name (required)">-->
-<!--                            </div>-->
-<!--                            <div class="col-md-6 my-1">-->
-<!--                                <input class="form-control" type="text" name="last_name" placeholder="Last Name (optional)">-->
-<!--                            </div>-->
-<!--                            <div class="col-md-12">-->
-<!--                                <div class="form-control my-2">-->
-<!--                                    <div class="row ">-->
-<!--                                        <h4 class="col-12">Attending?</h4>-->
-<!--                                    </div>-->
-<!--                                    <div class="form-check">-->
-<!--                                        <input class="form-check-input" type="radio" name="response_status" id="yes" value="ATTENDING" checked>-->
-<!--                                        <label class="form-check-label" for="yes">-->
-<!--                                            Yes-->
-<!--                                        </label>-->
-<!--                                    </div>-->
-<!--                                    <div class="form-check">-->
-<!--                                        <input class="form-check-input" type="radio" name="response_status" id="maybe" value="COWARD">-->
-<!--                                        <label class="form-check-label" for="maybe">-->
-<!--                                            Maybe-->
-<!--                                        </label>-->
-<!--                                    </div>-->
-<!--                                    <div class="form-check">-->
-<!--                                        <input class="form-check-input" type="radio" name="response_status" id="no" value="NO">-->
-<!--                                        <label class="form-check-label" for="no">-->
-<!--                                            No-->
-<!--                                        </label>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                            <div class="col-md-12">-->
-<!--                                <div class="talent-box form-control my-2">-->
-<!--                                    <div class="row ">-->
-<!--                                        <h4 class="col-12">Talent?</h4>-->
-<!--                                    </div>-->
-<!--                                    <div class="form-check">-->
-<!--                                        <input class="form-check-input" type="radio" name="talent" id="yes" value="1" checked>-->
-<!--                                        <label class="form-check-label" for="yes">-->
-<!--                                            Ye-->
-<!--                                        </label>-->
-<!--                                    </div>-->
-<!--                                    <div class="form-check">-->
-<!--                                        <input class="form-check-input" type="radio" name="talent" id="no" value="0">-->
-<!--                                        <label class="form-check-label" for="no">-->
-<!--                                            Nay-->
-<!--                                        </label>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="modal-footer">-->
-<!--                            <button type="submit" class="btn btn-primary text-end">Submit</button>-->
-<!--                        </div>-->
-<!--                    </Form>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
     </Layout>
 </template>
 
