47be7a08da10a21a16b2d3e1f48e5b9d406b29af
Tanner Wells <tannerjameswells@gmail.com>
2023-11-25 22:58:34 -0500

    update routes maybe??

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,17 @@ Route::get('/', function () {
     $show = \App\Models\Show::where('date', '>=', Carbon::now())->orderBy('date', 'asc')->first();
     return view('home', compact('show'));
 });
-Route::get('shows/{show}/view', 'ShowController@show')->name('shows.show');
-Route::get('shows/{show}/invite/respond/{key}', 'InviteController@respond');
-Route::post('shows/{show}/invite/respond/{key}', 'InviteController@registerResponse');
+Route::get('/shows/{show}/view', 'ShowController@show')->name('shows.show');
+Route::get('/shows/{show}/invite/respond/{key}', 'InviteController@respond');
+Route::post('/shows/{show}/invite/respond/{key}', 'InviteController@registerResponse');
 Route::get('/invite/{invite}/thank-you', 'InviteController@guestThankYou');
 Route::post('/invites/{invite}/generate-ics', 'InviteController@generateICS');
 Route::get('/invites/{invite}/edit', 'InviteController@edit');
 Route::post('/invites/{invite}/mark-as-sent', 'InviteController@markAsSent');
 Route::post('/invites/{invite}/mark-as-opened', 'InviteController@markAsOpened');
 
-Route::get('shows/{show}/submission-applications/create', 'SubmissionApplicationController@create');
-Route::post('shows/{show}/submission-applications/', 'SubmissionApplicationController@store');
+Route::get('/shows/{show}/submission-applications/create', 'SubmissionApplicationController@create');
+Route::post('/shows/{show}/submission-applications/', 'SubmissionApplicationController@store');
 Route::get('/shows/{show}/submission-applications/{submissionApplication}/view', 'SubmissionApplicationController@show');
 
 Route::get('/shows/{show}/invite/guest-request', 'InviteController@guestRequest')->name('invites.guest-request');
